1b68e81e3b38dfce7849aa3354d816cce1292a55
kangcheolung <skhu202114111@gmail.com>
2025-04-09 09:49:51 +0900

    five

--- src/c202114111/World.java
+++ src/c202114111/World.java
@@ -2,6 +2,9 @@ package c202114111;
 
 public class World { 
   public static void run() {
-     System.out.println("201514099"); 
-    } 
-  }
+     System.out.println("202114111"); 
+  } 
+  public static void run2() {
+    System.out.println("hello world");
+  } 
+}
